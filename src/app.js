60185f327907058341d683bc02206b39f30fa460
Ziegler <zieglering@gmail.com>
2024-04-19 03:57:33 -0300

    Arreglado el POST, y agregado DELETE, y PUT en tiempo real con socket.io

--- src/app.js
+++ src/app.js
@@ -1,78 +1,103 @@
-import express from 'express';
+import express from "express";
 
-import { __dirname } from './utils.js'
-import productsRouter from './routes/products.router.js'
-import realtimeproductsRouter from './routes/realtimeproducts.router.js'
-import cartsRouter from './routes/carts.router.js'
-import viewsRouter from './routes/views.router.js'
+import { __dirname } from "./utils.js";
+import productsRouter from "./routes/products.router.js";
+import realtimeproductsRouter from "./routes/realtimeproducts.router.js";
+import cartsRouter from "./routes/carts.router.js";
+import viewsRouter from "./routes/views.router.js";
 
-
-import handlebars from 'express-handlebars'
-import { Server } from 'socket.io'
-import ProductManager from './productManager.js';
-
+import handlebars from "express-handlebars";
+import { Server } from "socket.io";
+import ProductManager from "./productManager.js";
 
 const productsJsonPath = `${__dirname}/Products.json`;
 const productManager = new ProductManager(productsJsonPath);
 
-
-
-
 const app = express();
-const PORT = process.env.PORT || 8080
-
-
+const PORT = process.env.PORT || 8080;
 
 const httpServer = app.listen(PORT, (error) => {
-    if(error) return console.log(error);
-    console.log('Server escuchando en el puerto 8080');
-    
+	if (error) return console.log(error);
+	console.log("Server escuchando en el puerto 8080");
 });
 
-const io = new Server(httpServer)
- 
+const io = new Server(httpServer);
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static(`${__dirname}/public`))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(`${__dirname}/public`));
 
-app.engine('.handlebars', handlebars.engine())
+app.engine(".handlebars", handlebars.engine());
 
-app.set('views', `${__dirname}/views`)
-app.set('view engine', '.handlebars')
+app.set("views", `${__dirname}/views`);
+app.set("view engine", ".handlebars");
 
-app.use('/', viewsRouter)
-app.use('/api/products', productsRouter)
-app.use('/realtimeproducts', realtimeproductsRouter)
-app.use('/api/carts', cartsRouter)
+app.use("/", viewsRouter);
+app.use("/api/products", productsRouter);
+app.use("/realtimeproducts", realtimeproductsRouter);
+app.use("/api/carts", cartsRouter);
 
+io.on("connect", async (socket) => {
+	console.log("Cliente conectado");
 
-io.on('connection', async (socket) => {
-    console.log('Cliente conectado');
+	io.emit("getProducts", await productManager.getProducts());
 
+	socket.on("addNewProduct", async (newProduct) => {
+		try {
+			const response = await fetch("http://localhost:8080/api/products", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(newProduct),
+			});
 
-    io.emit('getProducts', await productManager.getProducts()); 
+			const responseData = await response.json();
+			io.emit("newProductAdded", responseData);
+		} catch (error) {
+			console.error("Error", error);
+		}
+		io.emit("getProducts", await productManager.getProducts());
+	});
 
-    socket.on('addNewProduct', async (newProduct) => {
-        try {
+	socket.on("deleteProduct", async (productID) => {
+		try {
+			const response = await fetch(
+				`http://localhost:8080/api/products/${productID}`,
+				{
+					method: "DELETE",
+					headers: {
+						"Content-Type": "application/json",
+					},
+				},
+			);
+			const responseData = await response.json();
+			socket.emit("deletedProduct", responseData);
+			socket.emit("getProducts", await productManager.getProducts());
+		} catch (error) {
+			console.error("Error", error);
+		}
+		io.emit("getProducts", await productManager.getProducts());
+	});
 
-            const response = await fetch('http://localhost:8080/api/products', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(newProduct)
-            });
-
-            const responseData = await response.json();
-            io.emit('newProductAdded', responseData);
-            io.emit('getProducts', await productManager.getProducts());
-        } catch (error) {
-            console.error('Error', error);
-            
-        }
-    });
-    
-    
-    
+	socket.on("updateProduct", async (productID, updatedProduct) => {
+		try {
+			const response = await fetch(
+				`http://localhost:8080/api/products/${productID}`,
+				{
+					method: "PUT",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify(updatedProduct),
+				},
+			);
+			const responseData = await response.json();
+			socket.emit("deletedProduct", responseData);
+			socket.emit("getProducts", await productManager.getProducts());
+		} catch (error) {
+			console.error("Error", error);
+		}
+		io.emit("getProducts", await productManager.getProducts());
+	});
 });
--- src/public/js/realtimeproducts.js
+++ src/public/js/realtimeproducts.js
@@ -1,69 +1,93 @@
-// import { io } from 'socket.io-client';
+const socket = io("http://localhost:8080");
 
+const addProductForm = document.querySelector("#addProductForm");
+const title = document.querySelector("#title");
+const description = document.querySelector("#description");
+const code = document.querySelector("#code");
+const price = document.querySelector("#price");
+const productStatus = document.querySelector("#status");
+const stock = document.querySelector("#stock");
+const category = document.querySelector("#category");
+const thumbnails = document.querySelector("#thumbnails");
 
-
-const socket = io('http://localhost:8080');
-
-socket.on('connect', () => {
-    console.log('Conectado al servidor Socket.IO');
-
+socket.on("connect", () => {
+	console.log("Conectado al servidor Socket.IO");
 });
-
-socket.on('getProducts', (products) => {
-  const listProducts = document.querySelector('#listProducts')
-  let product = ''
-  for (prod of products) {
-    product += `
+socket.on("getProducts", async (products) => {
+	const listProducts = document.querySelector("#listProducts");
+	let product = "";
+	for (prod of await products) {
+		product += `
     <div class="container">
       <li>${prod.title}</li>
+      <div>
+        <button class="btnDelete" id="${prod.id}">Borrar</button>
+      </div>
+      <div>
+        <button class="btnUpdate" id="${prod.id}">Actualizar</button>
+      </div>
     </div>
-    `
-  }
-  
-  listProducts.innerHTML = product
-})
+    `;
+	}
 
+	listProducts.innerHTML = product;
 
+	btnDelete = document.querySelectorAll(".btnDelete");
 
+	btnDelete.forEach((btn) => {
+		btn.addEventListener("click", async (evt) => {
+			evt.preventDefault();
+			socket.emit("deleteProduct", btn.id);
+		});
+	});
 
+	let updatedProduct = [];
+	btnUpdate = document.querySelectorAll(".btnUpdate");
+	btnUpdate.forEach((btn) => {
+		btn.addEventListener("click", async (evt) => {
+			evt.preventDefault();
 
-const addProductForm = document.querySelector('#addProductForm')
-const title = document.querySelector('#title')
-const description = document.querySelector('#description')
-const code = document.querySelector('#code')
-const price = document.querySelector('#price')
-const statuse = document.querySelector('#status')
-const stock = document.querySelector('#stock')
-const category = document.querySelector('#category')
-const thumbnails = document.querySelector('#thumbnails')
-const btnBorrar = document.querySelector('#btnBorrar')
+			const updatedProductData = {
+				title: title.value,
+				description: description.value,
+				code: code.value,
+				price: Number.parseInt(price.value),
+				status: Boolean(statusCheck),
+				stock: Number.parseInt(stock.value),
+				category: category.value,
+				thumbnails: thumbnails.value,
+			};
+
+			updatedProduct = updatedProductData;
+
+			socket.emit("updateProduct", btn.id, updatedProduct);
+		});
+	});
+});
+
+// console.log(productID)
+// socket.emit("deleteProduct", productID);
 
 statusCheck = () => {
-  if (statuse.checked) return console.log('true') 
-  return console.log('false') 
-}
+	if (productStatus.checked) return console.log("true");
+	return console.log("false");
+};
 
-let newProduct = []
+let newProduct = [];
 
-addProductForm.addEventListener('click', async (evt) => {
-  evt.preventDefault()
-  const newProductData = {
-      title: title.value,
-      description: description.value,
-      code: code.value,
-      price: Number.parseInt(price.value),
-      status: Boolean(statusCheck),
-      stock: Number.parseInt(stock.value),
-      category: category.value,
-      thumbnails: thumbnails.value
-      };
- 
-      newProduct = newProductData
-      socket.emit('addNewProduct', newProduct)
+addProductForm.addEventListener("click", async (evt) => {
+	evt.preventDefault();
+	const newProductData = {
+		title: title.value,
+		description: description.value,
+		code: code.value,
+		price: Number.parseInt(price.value),
+		status: Boolean(statusCheck),
+		stock: Number.parseInt(stock.value),
+		category: category.value,
+		thumbnails: thumbnails.value,
+	};
 
-      
-    });
-    
- 
-
-
+	newProduct = newProductData;
+	socket.emit("addNewProduct", newProduct);
+});
