3c67341f72609d6d3f7bda1cf70b3adb904124ea
Ziegler <zieglering@gmail.com>
2024-05-27 05:09:32 -0300

    Implementacion de Login

--- src/routes/api/chat.router.js
+++ src/routes/api/chat.router.js
@@ -1,28 +1,28 @@
-import { Router } from 'express'
-import ChatMongoManager from '../../daos/chatMongo.manager.js'
+import { Router } from 'express';
+import ChatMongoManager from '../../daos/chatManagerMongo.js';
 
-const router = Router()
-const chatService = new ChatMongoManager
+const router = Router();
+const chatService = new ChatMongoManager;
 
 router.get('/', async (req, res) => {
     try {
-        const messages = await chatService.getMessages()
-        res.status(200).send({status: 'success', payload: messages})
-        
+        const messages = await chatService.getMessages();
+        res.status(200).send({ status: 'success', payload: messages });
+
     } catch (error) {
-        throw error
+        throw error;
     }
-})
+});
 
 router.post('/', async (req, res) => {
     try {
-        const { user, message } = req.body
-        const newMessage = await chatService.addMessage(user, message)
-        res.send({status:'success', payload: newMessage})
-        
-    } catch (error) {
-        throw error
-    }
-})
+        const { user, message } = req.body;
+        const newMessage = await chatService.addMessage(user, message);
+        res.send({ status: 'success', payload: newMessage });
 
-export default router
+    } catch (error) {
+        throw error;
+    }
+});
+
+export default router;
